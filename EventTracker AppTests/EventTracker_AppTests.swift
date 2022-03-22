670158448488c40ec508e48ae568d58d5b2c05d3
Nitesh Isave <nitesh.isave@infinx.com>
2022-03-22 20:16:15 +0530

    Initial Commit

---
+++ EventTracker AppTests/EventTracker_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  EventTracker_AppTests.swift
+//  EventTracker AppTests
+//
+//  Created by Nitesh on 22/03/22.
+//
+
+import XCTest
+@testable import EventTracker_App
+
+class EventTracker_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
